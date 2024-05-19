3be1c2a7c02ce48178445036675a1f4dd3ed8121
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-05-19 13:33:09 +0200

    Initiate Challenge 61.

---
+++ Challenge 61 - Advanced forms with Flask-WTForms/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label='Login')
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+
+app = Flask(__name__)
+app.secret_key = "thatwashardenough"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
