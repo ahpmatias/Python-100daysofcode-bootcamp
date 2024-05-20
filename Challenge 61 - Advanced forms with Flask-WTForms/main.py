1485b0f2de96f28da6b52271cb7de172edc69c65
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-05-20 16:36:59 +0200

    Complete Challenge 61.

--- Challenge 61 - Advanced forms with Flask-WTForms/main.py
+++ Challenge 61 - Advanced forms with Flask-WTForms/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap5
+
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), validators.Email()])
@@ -25,6 +27,7 @@ This will install the packages from requirements.txt for this project.
 app = Flask(__name__)
 app.secret_key = "thatwashardenough"
 
+bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def home():
