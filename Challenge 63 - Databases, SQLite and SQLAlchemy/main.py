182d89d0b91894064499d57c3939a99b417b2d15
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-05-24 18:05:41 +0200

    Conclude Challenge 63.

--- Challenge 63 - Databases, SQLite and SQLAlchemy/main.py
+++ Challenge 63 - Databases, SQLite and SQLAlchemy/main.py
@@ -76,6 +76,27 @@ def add():
     
     return render_template('add.html', form=form)
 
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
