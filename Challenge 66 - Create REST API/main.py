3d59a1fc1ba4fc80b7a588d6b7279895fdd47b39
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-06-16 16:46:24 +0200

    Challenge 66 - finish implementation

--- Challenge 66 - Create REST API/main.py
+++ Challenge 66 - Create REST API/main.py
@@ -114,6 +114,20 @@ def update_cafes(cafe_id):
         return jsonify(response={'error':{'Not Found':'Sorry, a cafe with this id was not found in the database.'}})
 
 # HTTP DELETE - Delete Record
-
+@app.route('/report-closed/<int:cafe_id>', methods=['GET','DELETE'])
+def delete_cafes(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == 'TopSecretAPIKey':
+        try:
+            deleted_cafe = db.get_or_404(Cafe, cafe_id)
+            db.session.delete(deleted_cafe)
+            db.session.commit()
+            
+            return jsonify(response={'success': 'Successfully deleted cafe.'}), 200
+        except:
+            return jsonify(error={'Not Found':'Sorry, a cafe with this id was not found in the database.'}), 404
+    else:
+        return jsonify(error={'Forbidden':'Sorry, that is not allowed. Make sure you have the correct api_key.'}), 403
+    
 if __name__ == '__main__':
     app.run(debug=True)
